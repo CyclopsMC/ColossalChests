440b6eea56844b773654f5eab9ef173c63f93f34
Ruben Taelman <rubensworks@gmail.com>
2016-07-03 18:18:22 +0200

    Bump mod version

--- src/main/java/org/cyclops/colossalchests/Reference.java
+++ src/main/java/org/cyclops/colossalchests/Reference.java
@@ -35,7 +35,7 @@ public class Reference {
     public static final String MOD_FORGE_VERSION_MIN = "12.17.0.1909";
     public static final String MOD_CYCLOPSCORE = "cyclopscore";
     public static final String MOD_CYCLOPSCORE_VERSION = "@CYCLOPSCORE_VERSION@";
-    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.6.1";
+    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.7.0";
     public static final String MOD_IRONCHEST = "IronChest";
     
     // Dependencies

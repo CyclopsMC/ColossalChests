fcb8e7c5d64d81614162b04f775ab149d6bd8896
Ruben Taelman <rubensworks@gmail.com>
2016-10-05 20:23:33 +0200

    Use new CyclopsCore gui provider interface

--- src/main/java/org/cyclops/colossalchests/client/gui/container/GuiColossalChest.java
+++ src/main/java/org/cyclops/colossalchests/client/gui/container/GuiColossalChest.java
@@ -67,7 +67,7 @@ public class GuiColossalChest extends ScrollingGuiContainer {
 
     @Override
     public String getGuiTexture() {
-        return getContainer().getGuiProvider().getMod().getReferenceValue(ModBase.REFKEY_TEXTURE_PATH_GUI)
+        return getContainer().getGuiProvider().getModGui().getReferenceValue(ModBase.REFKEY_TEXTURE_PATH_GUI)
                 + "colossalChest.png";
     }
 

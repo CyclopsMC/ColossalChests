783c65e62416e64045bf448f13c79c9c38463db4
Ruben Taelman <rubensworks@gmail.com>
2020-04-14 18:00:25 +0200

    Fix uncolossal chest item rendering failing
    And sometimes causing a crash, such as with JEI.
    
    Closes #131

--- src/main/java/org/cyclops/colossalchests/client/render/tileentity/ItemStackTileEntityUncolossalChestRender.java
+++ src/main/java/org/cyclops/colossalchests/client/render/tileentity/ItemStackTileEntityUncolossalChestRender.java
@@ -1,31 +1,18 @@
 package org.cyclops.colossalchests.client.render.tileentity;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
-import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.cyclops.colossalchests.tileentity.TileUncolossalChest;
-
-import java.util.function.Supplier;
+import org.cyclops.cyclopscore.client.render.tileentity.ItemStackTileEntityRendererBase;
 
 /**
  * @author rubensworks
  */
 @OnlyIn(Dist.CLIENT)
-public class ItemStackTileEntityUncolossalChestRender extends ItemStackTileEntityRenderer {
-
-    private final TileUncolossalChest tile;
+public class ItemStackTileEntityUncolossalChestRender extends ItemStackTileEntityRendererBase {
 
     public ItemStackTileEntityUncolossalChestRender() {
-        this.tile = new TileUncolossalChest();
-    }
-
-    @Override
-    public void render(ItemStack itemStackIn, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int combinedLightIn, int combinedOverlayIn) {
-        TileEntityRendererDispatcher.instance.renderItem(this.tile, matrixStackIn, bufferIn, combinedLightIn, combinedOverlayIn);
+        super(TileUncolossalChest::new);
     }
 
 }

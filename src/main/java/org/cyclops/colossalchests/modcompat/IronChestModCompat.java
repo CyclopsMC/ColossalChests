81d901634fb174292d1ba5655eb221f05a3eb97f
Ruben Taelman <rubensworks@gmail.com>
2020-05-09 13:47:11 +0200

    Fix broken chest textures for Iron Chest compat, Closes #132

--- src/main/java/org/cyclops/colossalchests/modcompat/IronChestModCompat.java
+++ src/main/java/org/cyclops/colossalchests/modcompat/IronChestModCompat.java
@@ -19,19 +19,19 @@ public class IronChestModCompat implements IModCompat {
 
 	@OnlyIn(Dist.CLIENT)
 	private void overrideTextures() {
-		String prefix = "textures/model/";
+		String prefix = "model/";
 		RenderTileEntityColossalChest.TEXTURES_CHEST.put(ChestMaterial.COPPER,
-				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "copper_chest.png"));
+				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "copper_chest"));
 		RenderTileEntityColossalChest.TEXTURES_CHEST.put(ChestMaterial.IRON,
-				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "iron_chest.png"));
+				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "iron_chest"));
 		RenderTileEntityColossalChest.TEXTURES_CHEST.put(ChestMaterial.SILVER,
-				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "silver_chest.png"));
+				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "silver_chest"));
 		RenderTileEntityColossalChest.TEXTURES_CHEST.put(ChestMaterial.GOLD,
-				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "gold_chest.png"));
+				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "gold_chest"));
 		RenderTileEntityColossalChest.TEXTURES_CHEST.put(ChestMaterial.DIAMOND,
-				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "diamond_chest.png"));
+				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "diamond_chest"));
 		RenderTileEntityColossalChest.TEXTURES_CHEST.put(ChestMaterial.OBSIDIAN,
-				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "obsidian_chest.png"));
+				new ResourceLocation(Reference.MOD_IRONCHEST, prefix + "obsidian_chest"));
 	}
 
 	@Override

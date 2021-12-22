e1700c63f0a967d15ba719bdd640481b2171951f
Ruben Taelman <rubensworks@gmail.com>
2021-12-22 11:01:24 +0100

    Remove unused imports

--- src/main/java/org/cyclops/colossalchests/client/gui/container/ContainerScreenColossalChest.java
+++ src/main/java/org/cyclops/colossalchests/client/gui/container/ContainerScreenColossalChest.java
@@ -8,7 +8,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.core.NonNullList;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.network.protocol.game.ServerboundContainerClickPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
